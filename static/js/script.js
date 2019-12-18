16dbe0df753145916c434f69a3e324d50cf80dd6
coudert <jean.baptiste.coudert1@gmail.com>
2019-12-18 11:18:40 +0100

    finish_mothuerfuck_le_S_Gotze_noumero_19

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    var valueHtml1 = $('.state1').html();
+    // console.log(valueHtml);
+    var valueHtml2 = $('.state2').html();
+    var valueHtml3 = $('.state3').html();
+    
+    $('.backDark1').css("width", valueHtml1);
+    $('.backDark2').css("width", valueHtml2);
+    $('.backDark3').css("width", valueHtml3);
+    // valueHtml.change(function () {
+    // })
+})
